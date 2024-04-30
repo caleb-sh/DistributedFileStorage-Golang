09cb0b546d18591cf3dd7e9244dfd4a2b2553362
caleb-sh <calebloriago@gmail.com>
2024-04-30 11:18:21 -0600

    Primera hora del video

--- main.go
+++ main.go
@@ -6,6 +6,12 @@ import (
 )
 
 func main() {
+
+	tcpOpts := p2p.TCPTransportOpts{
+		ListenAddr:    ":3000",
+		HandshakeFunc: p2p.NOPHandshakeFunc,
+	}
+
 	tr := p2p.NewTCPTransport(":3000")
 
 	if err := tr.ListenAndAccept(); err != nil {
